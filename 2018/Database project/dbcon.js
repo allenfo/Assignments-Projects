090319150474d260e8bb324e774b8170dc32e539
allenfo <35274045+allenfo@users.noreply.github.com>
2019-04-01 15:28:41 -0500

    Update dbcon.js

--- 2018/Database project/dbcon.js
+++ 2018/Database project/dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs361_allenfo',
-  password        : '1124',
-  database        : 'cs361_allenfo'
+  host            : '**************************',
+  user            : '******************',
+  password        : '********************',
+  database        : '****************'
 });
 
 module.exports.pool = pool;
